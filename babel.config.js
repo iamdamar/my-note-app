60fc53a3648ac9cf296adcc46b9b9751d54dbaf6
Damar Wulan <79195688+iamdamar@users.noreply.github.com>
2024-06-19 04:13:30 +0000

    babel

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,18 @@
 module.exports = function (api) {
   api.cache(true);
+
+  const presets = [
+    '@babel/preset-env',
+    '@babel/preset-react',
+  ];
+
+  const plugins = [
+    '@babel/plugin-transform-runtime',
+    // Add any additional plugins needed
+  ];
+
   return {
-    presets: ['babel-preset-expo'],
+    presets,
+    plugins,
   };
 };
