613e57ae656f9bca0e99ee232e4a028295902b85
Damar Wulan <79195688+iamdamar@users.noreply.github.com>
2024-06-19 03:18:30 +0000

    update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
-module.exports = {
-    webpack: (config) => {
-      config.resolve.alias['react-native$'] = 'react-native-web';
-      return config;
-    },
-  };
-  
+const { withExpo } = require('@expo/next-adapter');
+const { resolve } = require('path');
+
+module.exports = withExpo({
+  projectRoot: __dirname,
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      crypto: require.resolve('crypto-browserify'),
+    };
+    return config;
+  },
+});
